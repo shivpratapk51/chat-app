42c101b08a3ecb47afe16c503021d505a1a69bc4
shivpratapk51 <shivpratao05oct2007@gmail.com>
2024-08-13 11:37:26 +0530

    work start

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -1,8 +1,22 @@
+import Background from "@/assets/login2.png";
+import Victory from "@/assets/victory.svg"
+
 const Auth = () => {
   return (
     <div className="h-[100vh] w-[100vw] flex items-center justify-center">
-      <div className="h-[80vh] bg-white border-white text-opacity-90 shadow-2xl w-[80vw]">
-
+      <div className="h-[80vh] bg-white border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:[70vw] xl:[60vw] rounded-3xl grid xl:grid-cols-2">
+        <div className="flex flex-col gap-10 items-center justify-center">
+          <div className="flex items-center justify-center flex-col">
+            <div className="flex justify-center items-center">
+              <h1 className="text-5xl font-bold md:text-6xl">Welcome</h1>
+              <img src={Victory} alt="victory emoji" className="h-[100px]" />
+            </div>
+            <p className="font-medium text-center">Fill in the details to get started with the best chat app!!</p>
+          </div>
+          <div className="flex items-center justify-center w-full">
+            
+          </div>
+        </div>
       </div>
     </div>
   )
