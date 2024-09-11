debfd9eea9de7bbf220248213a46b00e5f72fd6f
shivpratapk51 <shivpratao05oct2007@gmail.com>
2024-09-11 16:36:56 +0530

    server setup completed

--- server/index.js
+++ server/index.js
@@ -1,22 +1,31 @@
-import express from 'express'
-import dotenv from "dotenv"
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
-import mongoose from 'mongoose'
+import express, { json } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-const databaseURI = process.env.DATABASE_URI
+const databaseURI = process.env.DATABASE_URI;
 
-const dbConnection = mongoose.connect(process.env.DATABASE_URI)
+app.use(
+  cors({
+    origin: [process.env.ORIGIN],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
 
-const server = app.listen(port, ()=>{
-    console.log(`Server is running at http://localhost:${port}`);
-    console.log(dbConnection);
-    
-})
+app.use(cookieParser());
+app.use(express.json());
 
+const server = app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
 
-
+mongoose
+  .connect(databaseURI)
+  .then(() => console.log("DB connection successfully."))
+  .catch((err) => console.log(err.message));
